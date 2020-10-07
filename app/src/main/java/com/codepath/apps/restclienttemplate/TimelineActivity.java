02d0eb8dbf74864471803700f9ae16f578d6359b
Wallace-GS <wgsantos29@gmail.com>
2020-10-07 11:32:30 -0700

    implements oath

--- app/src/main/java/com/codepath/apps/restclienttemplate/TimelineActivity.java
+++ app/src/main/java/com/codepath/apps/restclienttemplate/TimelineActivity.java
@@ -1,6 +1,7 @@
 package com.codepath.apps.restclienttemplate;
 
 import android.os.Bundle;
+import android.util.Log;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,6 +11,7 @@ import okhttp3.Headers;
 
 public class TimelineActivity extends AppCompatActivity {
 
+    public static final String TAG = "TimelineActivity";
     TwitterClient client;
 
     @Override
@@ -25,12 +27,12 @@ public class TimelineActivity extends AppCompatActivity {
         client.getHomeTimeline(new JsonHttpResponseHandler() {
             @Override
             public void onSuccess(int statusCode, Headers headers, JSON json) {
-
+                Log.i(TAG, "onSuccess!");
             }
 
             @Override
             public void onFailure(int statusCode, Headers headers, String response, Throwable throwable) {
-
+                Log.i(TAG, "onFailure!", throwable);
             }
         });
     }
